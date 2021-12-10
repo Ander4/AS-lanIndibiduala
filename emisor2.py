f41bdcd6621849a0ef8a4da0ae85c77dfa70e6b8
Ander4 <49266554+Ander4@users.noreply.github.com>
2021-12-10 12:22:02 +0100

    subido emisor2.py para la imagen docker

---
+++ emisor2.py
@@ -0,0 +1,16 @@
+#/usr/bin/env python
+import pika
+
+credentials = pika.PlainCredentials(username='guest', password='guest')
+parameters= pika.ConnectionParameters('localhost', 5672, '/', credentials)
+connection = pika.BlockingConnection(parameters)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+mensaje='Enhorabuena, eres el cliente 1.000.000, accede en el siguiente enlace para reclamar tu premio: https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+
+channel.basic_publish(exchange='', routing_key='hello', body=mensaje)
+print(" [x] Sent ",mensaje)
+connection.close()
